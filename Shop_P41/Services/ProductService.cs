829da08c10ddc6ca21c9e3058377fb01b553c551
Mario <mario@gmail.com>
2026-05-22 19:01:17 +0300

    gfdgfdg

--- Shop_P41/Services/ProductService.cs
+++ Shop_P41/Services/ProductService.cs
@@ -1,7 +1,13 @@
-﻿public interface IProductService
+﻿using Microsoft.EntityFrameworkCore;
+
+public interface IProductService
 {
     //Додати методи CRUD для продукту
-    public IEnumerable<Product> GetProducts();
+    public Task<IEnumerable<Product>> GetProductsAsync();
+    public Task<Product> GetProductByIdAsync(Guid id);
+    public Task<Product> CreateAsync(Product product);
+    public Task<Product> UpdateAsync(int id,  Product product);
+    public Task<Product> DeleteAsync(int id);
 }
 
 public class ProductService : IProductService
@@ -12,7 +18,30 @@ public class ProductService : IProductService
         _context = context;
     }
 
-    public IEnumerable<Product> GetProducts()
+    public Task<Product> CreateAsync(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> GetProductByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Product>> GetProducts()
+        => await _context.Products.ToListAsync();
+
+    public Task<IEnumerable<Product>> GetProductsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> UpdateAsync(int id, Product product)
     {
         throw new NotImplementedException();
     }
