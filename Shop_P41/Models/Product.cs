dc5d5b14406a9345597464f83f84064135189fab
Mario <mario@gmail.com>
2026-05-22 17:20:05 +0300

    Add project files.

---
+++ Shop_P41/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Product
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string? Name { get; set; }
+    [Required]
+    [Precision(10, 2)]
+    public decimal Price { get; set; }
+    [Required]
+    [StringLength(1024)]
+    public string? Description { get; set; }
+    [Required]
+    [Range(0, int.MaxValue)]
+    public int Quantity { get; set; }
+    [Required]
+    [Range(0, int.MaxValue)]
+    public int CategoryId { get; set; }
+    //Navigation properties
+    public Category? Category { get; set; }
+    public ICollection<OrderItem>? OrderItems { get; set; }
+    public ICollection<CartItem>? CartItems { get; set; }
+    public ICollection<Review>? Reviews { get; set; }
+    public ICollection<ProductImage>? ProductImages { get; set; }
+}
