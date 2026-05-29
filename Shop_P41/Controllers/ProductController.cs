c6c4803de4c14c3eaf7bb4844c90753811475e21
Mario <mario@gmail.com>
2026-05-29 17:38:13 +0300

    258258

---
+++ Shop_P41/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop_P41.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetProductsAsync();
+            return View(products);
+        }
+    }
+}
--- Shop_P41/Program.cs
+++ Shop_P41/Program.cs
@@ -10,7 +10,13 @@ namespace Shop_P41
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddDbContext<ShopContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddScoped<IProductService, ProductService>();
+            builder.Services.AddControllersWithViews();
+
             var app = builder.Build();
+
+            app.UseRouting();
+            app.UseStaticFiles();
 
             app.Run();
         }
